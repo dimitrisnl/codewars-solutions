d7cb9692652c148e8a0f2e280eea566f01437b4b
Dimitrios Lytras <dnlytras@gmail.com>
2018-02-21 15:59:54 +0200

    time bomb challenge

---
+++ Hard Time Bomb/index.js
@@ -0,0 +1,13 @@
+// peaked in Bomb.toString implementation...
+const wireCode =
+  global['boom1'] ||
+  global['boom2'] ||
+  global['boom3'] ||
+  global['boom4'] ||
+  global['boom5'] ||
+  global['boom6'] ||
+  global['boom7'] ||
+  global['boom8'] ||
+  global['boom9'];
+
+Bomb.CutTheWire(wireCode);
